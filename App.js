2e07059466ea19d65607f1afc0e9599969110ee6
Don-Stevenson <dsteven1@gmail.com>
2021-01-28 18:27:53 -0500

    add accordlistitem import, clean up css

--- App.js
+++ App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
-import Header from "./components/Header"
+import React, { useState } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import Header from "./components/Header";
+import AccordionListItem from "./components/AccordionListItem"
+
+// useState 
+// useEffect to trigger the code the call change state
 
 export default function App() {
+  // const [rockets, setRockets] = useState([]);
+
   return (
     <View style={styles.container}>
-      <Header/>
-      <Text style={styles.text}>STAY OPEN Tech challenge</Text>
-        
+      <Header />
+      <AccordionListItem title={"List Rocket"}>
+        <Text>Some body text!</Text>
+      </AccordionListItem>
+      <Text style={styles.text}>Other Body Text</Text>
     </View>
-)}
-
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 60,
+    padding: 30,
+    justifyContent: 'flex-start'
   },
   text: {
-    color: 'darkslateblue',
-    fontSize: 30
+    color: "darkslateblue",
+    fontSize: 16,
   },
- });
+});
