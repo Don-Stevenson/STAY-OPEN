d99880efd94623922a7440e9ce50085f55ec0423
Don-Stevenson <dsteven1@gmail.com>
2021-01-30 19:59:25 -0500

    on select display description, animation not working yet

--- components/AccordionListItem.js
+++ components/AccordionListItem.js
@@ -1,75 +1,102 @@
 import React, { useState, useRef, useEffect } from "react";
-import {
-  StyleSheet,
-  View,
-  Text,
-  TouchableWithoutFeedback,
-  Animated,
-  Easing,
-  TouchableOpacity,
-} from "react-native";
-import { MaterialIcons } from "@expo/vector-icons";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { Transition, Transitioning } from "react-native-reanimated";
+
+// animate next seems to be fine according to the docs reanimated on mobile vew
+// setup the animation characteristics
+const transitionObj = (
+  <Transition.Together>
+    <Transition.In type="fade" durationMs={200} />
+    <Transition.Change />
+    <Transition.Out type="fade" durationMs={200} />
+  </Transition.Together>
+);
 
 export default function AccordionListItem({ title, children }) {
+  const [currentMissionId, setCurrentMissionId] = useState(null);
   const [missions, setMissions] = useState([]);
-  const [open, setOpen] = useState(false);
-  const animatedController = useRef(new Animated.Value(0)).current;
-  const [bodSectionHeight, setBodySectionHeight] = useState();
+  const ref = useRef();
 
   const fetchData = async () => {
     try {
       const response = await fetch(`https://api.spacexdata.com/v3/missions/`);
       const responseJSON = await response.json();
-      // console.log(responseJSON);
       setMissions(responseJSON);
-      // console.log("missions", missions);
     } catch (error) {
       console.log(error);
     }
-    // console.log('response:', response)
-    // return response;
   };
 
   useEffect(() => {
     fetchData();
-    // console.log({ missions });
   }, []);
 
-  console.log("missions", missions);
-
   return (
-    <View style={styles.bodyContainer}>
-      {missions.map(({ mission_name, description }) => {
+    <Transitioning.View
+      ref={ref}
+      transition={transitionObj}
+      style={styles.bodyContainer}
+    >
+      {missions.map((mission, index) => {
         return (
           <TouchableOpacity
-            key={mission_name}
-            onPress={() => {}}
+            key={mission.mission_id}
+            onPress={() => {
+              // this is problematic-  old way of calling the next transition
+              // look at docs for better examples
+              //  https://docs.swmansion.com/react-native-reanimated/docs/animations
+              ref.current.animateNextTransition();
+              setCurrentMissionId(mission.mission_id);
+            }}
             style={styles.cardContainer}
             activeOpacity={0.9}
           >
-            <View style={[styles.card, {backgroundColor: "darkblue" }]}>
-              <Text style={[styles.heading, {color: "pink"}]}>{mission_name}</Text>
+            <View style={[styles.card, { backgroundColor: "darkblue" }]}>
+              <Text style={[styles.heading, { color: "pink" }]}>
+                {mission.mission_name}
+              </Text>
+              {mission.mission_id === currentMissionId && (
+                <View style={styles.descriptionList}>
+                  <Text
+                    key={"description_" + index}
+                    style={[styles.body, { color: "yellow" }]}
+                  >
+                    {mission.description}
+                  </Text>
+                </View>
+              )}
             </View>
           </TouchableOpacity>
         );
       })}
-    </View>
+    </Transitioning.View>
   );
 }
 
 const styles = StyleSheet.create({
   cardContainer: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   card: {
     flexGrow: 1,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   heading: {
     fontSize: 38,
-    fontWeight: 900,
+    fontWeight: "900",
     textTransform: "uppercase",
     letterSpacing: -2,
+  },
+  descriptionList: {
+    fontSize: 20,
+    fontWeight: "500",
+    textTransform: "lowercase",
+    letterSpacing: 0,
+  },
+  body: {
+    fontSize: 20,
+    lineHeight: 20 * 1.5,
+    textAlign: "center",
   },
 });
